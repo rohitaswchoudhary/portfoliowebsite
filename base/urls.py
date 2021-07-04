9b5fb618fb0eeaba454ecb072da65e98f5c5ae2a
rohitaswchoudhary <rohitaswchoudhary@gmail.com>
2021-07-04 22:22:49 +0530

    added create project functionality

--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.homePage, name='home'),
+    
+    path('project/<str:pk>/', views.projectPage, name="project"),
+
+    path('add-project/', views.addProject, name="add-project"),
+    
 ]
